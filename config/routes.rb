fbdabf770ddb43b9bfa1cb8c1a2d40b89f810d28
Josh Blumberg <jlblumberg@gmail.com>
2020-04-01 16:24:36 +0100

    Update routes to use resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'friendships#index'
-  post 'friendships/new'
-  get 'friendships/create'
   devise_for :users
+  resources :friendships
+  resources :users
+  root 'friendships#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
