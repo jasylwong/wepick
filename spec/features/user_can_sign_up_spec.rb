6234ae96b7133382efec2fe40806e098b38ec614
hansa-lallu <hansalallu@gmail.com>
2020-04-01 12:19:21 +0100

    test for password character length

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -6,4 +6,9 @@ RSpec.feature "Sign up", type: :feature do
     sign_up("test@example.com", "example")
     expect(current_path).to eq('/users')
   end
+
+  scenario "User can't sign up with something with password less than 6 (inclusive) characters" do
+    sign_up("test@example.com", "12345")
+    expect(page).to have_content("Password is too short")
+  end
 end 
