f2b4af92c223a80393c1ed445312f1091e90b440
Jason <jasylwong@gmail.com>
2020-04-01 16:41:46 +0100

    pass can see users test

--- spec/features/user_can_see_users_spec.rb
+++ spec/features/user_can_see_users_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 RSpec.feature 'User can see users', type: :feature do
   scenario 'User can see a list of users' do
+    User.create(id: 1, email: 'signin_test@example.com', password: '123456')
     visit '/users/sign_up'
     fill_in('Email', with: 'test@example.com')
     fill_in('Password', with: 'password')
     fill_in('Password confirmation', with: 'password')
     click_button 'Sign up'
-    expect(page).to have_content('Welcome')
+    expect(page).to have_content('signin_test@example.com')
   end
 end
