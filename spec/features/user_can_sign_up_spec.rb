d8b7dde8d086870e96ec6439ae429c7c9d3b1af5
hansa-lallu <hansalallu@gmail.com>
2020-04-02 03:20:40 +0100

    error when user does not fill in pw confirmation during sign up test

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Sign up", type: :feature do
   
   scenario 'User can sign up' do
     sign_up("test@example.com", "example")
-    expect(current_path).to eq('/users')
+    expect(current_path).to eq('/users') # this test is currently passing as we need to go to a new route not '/users'
   end
 
   scenario "User can't sign up with something with password less than 6 (inclusive) characters" do
@@ -22,6 +22,14 @@ RSpec.feature "Sign up", type: :feature do
     fill_in "user_email", with: "test@example.com"
     fill_in "user_password", with: "password"
     fill_in "user_password_confirmation", with: "password2"
+    click_button("Sign up")
+    expect(page).to have_content("Password confirmation doesn't match Password")
+  end 
+
+  scenario "User does not fill in password confirmation when signing up" do 
+    visit('/users/sign_up')
+    fill_in "user_email", with: "test@example.com"
+    fill_in "user_password", with: "password"
     click_button("Sign up")
     expect(page).to have_content("Password confirmation doesn't match Password")
   end 
