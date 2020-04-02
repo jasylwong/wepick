14885115c5de4815e1cadcf333ced5b3d21ed96d
Josh Blumberg <jlblumberg@gmail.com>
2020-04-02 09:16:49 +0100

    Add friendship controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'friendship/index'
+  get 'friendship/create'
+  get 'friendship/new'
+  get 'friendship/edit'
+  get 'friendship/show'
+  get 'friendship/update'
+  get 'friendship/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
