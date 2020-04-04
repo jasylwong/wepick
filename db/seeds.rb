c7f9fa28fdb98fdd35719e22de10eac170db73c6
Josh Blumberg <jlblumberg@gmail.com>
2020-04-04 08:40:51 +0100

    Refactor genre hash in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,11 @@
 
 @collection = Tmdb::Collection.discover.args["results"]
 
-genre_list =
-    {
-      28 => "Action", 12 => "Adventure", 16 => "Animation", 35 => "Comedy", 80 => "Crime", 99 => "Documentary", 18 => "Drama", 10751 => "Family",
-    14 => "Fantasy", 36 => "History", 27 => "Horror", 10402 => "Music", 9648 => "Mystery", 10749 => "Romance", 878 => "Science Fiction", 10770 => "TV Movie",
-      53 => "Thriller", 10752 => "War", 37 => "Western" }
+genre_list = {  28 => "Action", 12 => "Adventure", 16 => "Animation", 35 => "Comedy",
+                80 => "Crime", 99 => "Documentary", 18 => "Drama", 10751 => "Family",
+                14 => "Fantasy", 36 => "History", 27 => "Horror", 10402 => "Music", 
+                9648 => "Mystery", 10749 => "Romance", 878 => "Science Fiction", 
+                10770 => "TV Movie", 53 => "Thriller", 10752 => "War", 37 => "Western" }
 
 @collection.each do |movie|
   # Some movies have >1 genre. Have kept as an array in the tables so can still be manipulated easily.
