10cd742d8ec1d8feb318df6f474c5a6133b5d910
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-04-05 12:24:17 +0100

    Changing spec to see Travis build

--- spec/features/user_can_see_a_movie_spec.rb
+++ spec/features/user_can_see_a_movie_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'viewing movie', type: :feature do
   scenario 'User can see a movie' do 
-    User.create(id: 3, email: 'signin_test@example.com', password: '123456')
+    User.create(id: 10, email: 'signin_test@example.com', password: '123456')
     Movie.create(id: 1, title: 'Lion King', imdb_id: 'tt6105098', overview: 'test description', poster_path: 'https://image.tmdb.org/t/p/w500/testposter.jpg')
     expect(Movie.find(1)).to be_a(Movie)
     create_test_user_and_login('signin_test2@example.com', '123456')
