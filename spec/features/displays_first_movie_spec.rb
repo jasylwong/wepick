76b193d175090583358995995191c81dd9f1e77b
hansa-lallu <hansalallu@gmail.com>
2020-04-06 03:31:40 +0100

    added db cleaner gem, reassigned ids in spec to variables

--- spec/features/displays_first_movie_spec.rb
+++ spec/features/displays_first_movie_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.feature 'Resets to the first movie', type: :feature do
   scenario 'Movie resets to first movie, after clicking rematch button' do
-    User.create(id: 12, email: 'signin_test@example.com', password: '123456')
-    Movie.create(id: 1, title: 'Lion King', imdb_id: 'tt6105098', overview: 'test description', poster_path: '/testposter.jpg')
-    Movie.create(id: 2, title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg')
-    Movie.create(id: 3, title: 'Bambi', imdb_id: 'tt6105024', overview: 'test description', poster_path: '/testposter.jpg')
-    MovieLike.create(id: 2, movie_id: 2, user_id: 12)
+    @user = User.create(email: 'signin_test@example.com', password: '123456')
+    Movie.create(title: 'Lion King', imdb_id: 'tt6105098', overview: 'test description', poster_path: '/testposter.jpg')
+    @movie = Movie.create(title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg')
+    Movie.create(title: 'Bambi', imdb_id: 'tt6105024', overview: 'test description', poster_path: '/testposter.jpg')
+    MovieLike.create(movie_id: @movie.id, user_id: @user.id)
+    
     create_test_user_and_login('signin_test2@example.com', '123456')
     click_link('Add friend', match: :first)
     click_link('Start Game')
