5d83f509ae4f5545a3ee3b1933b23a9fa05b1b9a
Joe Ellingworth <joeellingworth@gmail.com>
2020-04-06 10:32:11 +0100

    refactored tests, removed instance variables

--- spec/features/user_can_rematch_spec.rb
+++ spec/features/user_can_rematch_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.feature 'Rematch button', type: :feature do
   scenario 'once matched, user can see the rematch button' do
-    @user = User.create(email: 'signin_test@example.com', password: '123456')
+    user = User.create(email: 'signin_test@example.com', password: '123456')
     Movie.create(title: 'Lion King', imdb_id: 'tt6105098', overview: 'test description', poster_path: '/testposter.jpg')
-    @movie = Movie.create(title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg')
+    movie = Movie.create(title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg')
     Movie.create(title: 'Bambi', imdb_id: 'tt6105024', overview: 'test description', poster_path: '/testposter.jpg')
-    MovieLike.create(movie_id: @movie.id, user_id: @user.id)
+    MovieLike.create(movie_id: movie.id, user_id: user.id)
 
-    create_test_user_and_login('signin_test2@example.com', '123456', @movie.id - 2)
+    create_test_user_and_login('signin_test2@example.com', '123456', movie.id - 2)
     click_link('Add friend', match: :first)
     click_link('Start Game')
     click_button('Dislike')
@@ -17,14 +17,14 @@ RSpec.feature 'Rematch button', type: :feature do
   end
 
   scenario 'There is no retmach button when two users do not match' do
-    @user = User.create(email: 'signin_test@example.com', password: '123456')
+    user = User.create(email: 'signin_test@example.com', password: '123456')
     Movie.create(title: 'Lion King', imdb_id: 'tt6105098', overview: 'test description', poster_path: '/testposter.jpg')
-    @movie = Movie.create(title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg')
+    movie = Movie.create(title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg')
     Movie.create(title: 'Bambi', imdb_id: 'tt6105024', overview: 'test description', poster_path: '/testposter.jpg')
     Movie.create(title: 'Snow white', imdb_id: 'tt6105000', overview: 'test description', poster_path: '/testposter.jpg')
-    MovieLike.create(movie_id: @movie.id, user_id: @user.id)
+    MovieLike.create(movie_id: movie.id, user_id: user.id)
     
-    create_test_user_and_login('signin_test2@example.com', '123456', @movie.id - 2)
+    create_test_user_and_login('signin_test2@example.com', '123456', movie.id - 2)
     click_link('Add friend', match: :first)
     click_link('Start Game')
     click_button('Dislike')
