941cf5c0297b6a004daf5479aa5bfc48feb873a3
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-04-07 17:33:26 +0100

    Add test for clicking on Quit Game button to direct to friendships/show path

--- spec/features/users_can_like_movie_spec.rb
+++ spec/features/users_can_like_movie_spec.rb
@@ -14,4 +14,18 @@ RSpec.feature 'liking movies', type: :feature do
     expect(MovieLike.count).to eq(1)
     expect(User.find_by(email: 'signin_test2@example.com').movie_counter).to eq(1)
   end
+
+  scenario 'User can click on Quit Game button in nav bar' do
+    User.create(email: 'signin_test@example.com', password: '123456')
+    Movie.create(id: 1, title: 'Lion King', imdb_id: 'tt6105098', overview: 'test description', poster_path: '/testposter.jpg', genre: ["Action"])
+    Movie.create(id: 2, title: 'Aladdin', imdb_id: 'tt6105038', overview: 'test description', poster_path: '/testposter.jpg', genre: ["Action"])
+    create_test_user_and_login('signin_test2@example.com', '123456')
+    click_link('Add a friend')
+    click_link('Add friend', match: :first)
+    click_link('Start Game')
+    click_button('Action')
+    click_button('Like')
+    click_link('Quit Game')
+    expect(current_path).to eq('/friendships/show')
+  end
 end
