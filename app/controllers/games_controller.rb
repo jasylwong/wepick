29c5496753a98b46c25420c5d8b95429d1fd8b29
Joe Ellingworth <joeellingworth@gmail.com>
2020-04-07 16:47:17 +0100

    changed all @@friend to session[friend_id]

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,9 +2,9 @@ class GamesController < ApplicationController
   include GamesHelper
 
   def new
-    @@friend_id = params[:friend_id]
+    session[:friend_id] = params[:friend_id]
     # "To be deleted:"
-    @friend_id = @@friend_id
+    @friend_id = session[:friend_id]
     MovieLike.where(user_id: current_user.id).destroy_all
     User.where(id: current_user.id).update_all(movie_counter: 0)
     redirect_to '/games/preferences'
@@ -17,22 +17,22 @@ class GamesController < ApplicationController
     session[:genre] = params[:genre] unless params[:genre].nil?
     movies_by_genre = Movie.where('genre LIKE ?', "%#{session[:genre]}%").to_a
     movies_id_arr = movies_by_genre.map { |movie| movie.id }
-    @match, @movie = matcher(@@friend_id, movies_id_arr)
+    @match, @movie = matcher(session[:friend_id], movies_id_arr)
     # "To be deleted:"
-    @friend_id = @@friend_id
+    @friend_id = session[:friend_id]
   end
 
   def preferences
     # "To be deleted:"
-    @friend_id = @@friend_id
+    @friend_id = session[:friend_id]
   end 
 
   def destroy
     movie_counter = 0
     User.where(id: params[:user_id]).update_all(movie_counter: movie_counter)
     MovieLike.where(user_id: params[:user_id]).destroy_all
-    User.where(id: @@friend_id).update_all(movie_counter: movie_counter)
-    MovieLike.where(user_id: @@friend_id).destroy_all
+    User.where(id: session[:friend_id]).update_all(movie_counter: movie_counter)
+    MovieLike.where(user_id: session[:friend_id]).destroy_all
     redirect_to '/friendships/show'
   end
 
