c908255b7066f5312007153f85a15ce460d14afd
Joe Ellingworth <joeellingworth@gmail.com>
2020-04-08 11:23:42 +0100

    fixed create method

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,7 +6,7 @@ class FriendshipsController < ApplicationController
   def create
     @requesting_user = current_user
     @receiving_user = User.find_by(id: params[:friend])
-    unless @requesting_user.friends_with?(@requesting_user)
+    unless @requesting_user.friends_with?(@receiving_user)
       @requesting_user.friend_request(@receiving_user)
       @receiving_user.accept_request(@requesting_user)
     end 
